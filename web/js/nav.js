dd89e77015925bb2dae60962bee57d2bd2cacd05
Zach Clare2 <tclare50@gmail.com>
2022-12-19 16:55:51 +0000

    button style

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,4 +1,10 @@
 function toggleNav() {
+
+    // show/hide menu
     document.getElementById("nav__main").classList.toggle("d-none");
     document.getElementById("nav__main").classList.toggle("nav__mobile-menu");
+
+    // toggle icon between bars and X
+    document.getElementById("nav__burger-icon").classList.toggle("fa-bars");
+    document.getElementById("nav__burger-icon").classList.toggle("fa-xmark");
 }
