4971631dbf1406071043f28e67b6b056d77e121c
Zach Clare2 <tclare50@gmail.com>
2022-12-20 17:59:06 +0000

    homepage

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,10 +1,63 @@
 function toggleNav() {
 
-    // show/hide menu
+    var hidden;
+    if (document.getElementById("nav__main").classList.contains("d-none")) {
+        // currently hidden
+        hidden = true;
+    } else {
+        // currently showing
+        hidden = false;
+    }
+
+    // // show/hide menu
     document.getElementById("nav__main").classList.toggle("d-none");
     document.getElementById("nav__main").classList.toggle("nav__mobile-menu");
+
+    var hero = null;
+    try {
+        hero = heroVisible(document.getElementById("hero__title"));
+    } catch {
+        
+    }
+
+    if (hidden) { // menu is going to be shown
+        showTitle();
+    } else if (!hidden && !hero) {
+        hideTitle();
+    } else if (!hidden && hero) {
+        showTitle();
+    }
 
     // toggle icon between bars and X
     document.getElementById("nav__burger-icon").classList.toggle("fa-bars");
     document.getElementById("nav__burger-icon").classList.toggle("fa-xmark");
+    document.getElementById("nav__burger-icon").classList.toggle("rotate");
+}
+
+window.addEventListener("scroll", function() {
+    try {
+        var targetElement = document.getElementById("hero__title");
+    } catch {
+        return;
+    }
+    
+    if (heroVisible(targetElement)) {
+        showTitle();
+    } else {
+        hideTitle();
+    }
+});
+
+function hideTitle() {
+    var siteTitle = document.getElementById("nav__site-title");
+    siteTitle.classList.add("d-none");
+}
+
+function showTitle() {
+    var siteTitle = document.getElementById("nav__site-title");
+    siteTitle.classList.remove("d-none");
+}
+
+function heroVisible(targetElement) {
+    return this.window.scrollY > (targetElement.offsetTop + targetElement.offsetHeight);
 }
