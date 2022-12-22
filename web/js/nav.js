3abdb3853db93d4ecf39c6fae34153d8ec45ac6e
Zach Clare2 <tclare50@gmail.com>
2022-12-22 18:51:52 +0000

    desktop version of homepage

--- web/js/nav.js
+++ web/js/nav.js
@@ -9,13 +9,17 @@ function toggleNav() {
         hidden = false;
     }
 
-    // // show/hide menu
+    // show/hide menu
     document.getElementById("nav__main").classList.toggle("d-none");
     document.getElementById("nav__main").classList.toggle("nav__mobile-menu");
 
     var hero = null;
     try {
-        hero = heroVisible(document.getElementById("hero__title"));
+        var title = document.getElementById("hero__title--mobile");
+        if (title.style.display == "none") {
+            title = document.getElementById("hero__title");
+        }
+        hero = visible(title);
     } catch {
         
     }
@@ -36,12 +40,16 @@ function toggleNav() {
 
 window.addEventListener("scroll", function() {
     try {
-        var targetElement = document.getElementById("hero__title");
+        var title = document.getElementById("hero__title--mobile");
+        var titleStyles = this.getComputedStyle(title);
+        if (titleStyles.display == "none") {
+            title = document.getElementById("hero__title");
+        }
     } catch {
-        return;
+
     }
     
-    if (heroVisible(targetElement)) {
+    if (visible(title)) {
         showTitle();
     } else {
         hideTitle();
@@ -49,21 +57,21 @@ window.addEventListener("scroll", function() {
 });
 
 function hideTitle() {
-    var siteTitle = document.getElementById("nav__site-title");
-    siteTitle.classList.add("d-none");
+    document.getElementById("nav__site-title--mobile").style.opacity = 0;
+    document.getElementById("nav__site-title").style.opacity = 0;
 
     // remove filled in class
     document.getElementById("nav__mobile-bar").classList.remove("nav__mobile-bar--filled");
 }
 
 function showTitle() {
-    var siteTitle = document.getElementById("nav__site-title");
-    siteTitle.classList.remove("d-none");
+    document.getElementById("nav__site-title--mobile").style.opacity = 1;
+    document.getElementById("nav__site-title").style.opacity = 1;
 
     // add filled in class
     document.getElementById("nav__mobile-bar").classList.add("nav__mobile-bar--filled");
 }
 
-function heroVisible(targetElement) {
+function visible(targetElement) {
     return this.window.scrollY > (targetElement.offsetTop + targetElement.offsetHeight);
 }
